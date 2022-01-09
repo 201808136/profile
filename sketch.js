9bf0a804ae476fecdb81205633d5cca6f1e7ccf3
201808136 <93645241+201808136@users.noreply.github.com>
2022-01-09 19:41:35 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 //código base para guardar a imagem https://www.youtube.com/watch?v=9Rhsb3GU2Iw
+$("#submit").hide();
 
 function setup() {
   //sem canvas
@@ -6,12 +7,16 @@ function setup() {
   //quando carrega no botão é que aparece a câmara para tirar a fotografia
   const buttons = document.getElementById('change');
   buttons.addEventListener('click', async event => {
+    $("#submit").show();
+    $("#change").hide();
   //criar a captura do video
   const video = createCapture(VIDEO);
   video.size(160, 160);
   //ao carregar no botão para guardar
   const button = document.getElementById('submit');
   button.addEventListener('click', async event => {
+    $("#submit").hide();
+  $("#change").show();
    //guarda os pixeis do vídeo, ou seja a imagem
     video.loadPixels();
     //converte em Base64 através do toDataURL
@@ -22,7 +27,8 @@ function setup() {
     //mudar o css das imagens para o data da imagem criada
     $("#me").attr("src", image64);
     $("#me2").attr("src", image64);
-
+//fazer o video desaparecer depois de tirar a fotografia
+    $("video").remove();
   });
 });
 }
@@ -30,3 +36,5 @@ function setup() {
 const imag = localStorage.getItem("prf");
 $("#me").attr("src", imag);
 $("#me2").attr("src", imag);
+$("#me3").attr("src", imag);
+
